83004d42251e04701dc3136ebb60fd8f75b9c523
chuthanhphong <81786056+chuthanhphong@users.noreply.github.com>
2021-07-21 23:20:18 +0700

    add .

---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,82 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/create")
+    public ModelAndView showFormCreate() {
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("product",new Product());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView create(Product product) {
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("product",new Product());
+        return modelAndView;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView listCustomers() {
+        Iterable<Product> products = productService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+     Optional<Product> product = productService.findById(id);
+        if (product != null) {
+            ModelAndView modelAndView = new ModelAndView("/product/edit");
+            modelAndView.addObject("product", product);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/product/error");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-product")
+    public ModelAndView updateCustomer(@ModelAttribute("product") Product product) {
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/product/edit");
+        modelAndView.addObject("product", product);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+            Optional<Product> product = productService.findById(id);
+        if ( product!= null) {
+            ModelAndView modelAndView = new ModelAndView("/product/delete");
+            modelAndView.addObject("product", product.get());
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/product/error");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-product")
+    public String deleteCustomer(@ModelAttribute("product") Product product) {
+        productService.remove(product.getId());
+        return "redirect:/products/list";
+    }
+}
+
+
